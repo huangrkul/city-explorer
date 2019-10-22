d0e2ed69a8746f848dc0dff8b5cecfb2b411b3c6
huangrkul <huangrkul@gmail.com>
2019-10-22 10:04:11 -0700

    object constructed

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
 'use strict';
 
+//all required
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001; //reading port 3000 from .env
-const cors = require('cors');
-
 app.use(cors());
 
-app.get('/', (request, response) => response.send('Hello World!'))
+const PORT = process.env.PORT || 3001; //reading port 3000 from .env
+
+app.get('/location', (request, response) => {
+  try{
+    const city = request.query.data;
+    const locationData = fetchLatLong(city);
+
+    console.log(locationData);
+    response.send(locationData);
+  }
+  catch(error){
+    console.error(error);
+    response.status(500).send('server issue (500)');
+  }
+})
+
+//app.get('/', (request, response) => response.send('Hello World!'))
+
+//error msg handling for status 404
 app.get('*',(request, response) => {
   response.status(404).send('not found');
 })
 
+//this function takes location data submitted from user query and instantiate a series of objects.
+function fetchLatLong(location){
+  const geoData = require('./data/geo.json');
+  console.log(geoData);
+  const locationObj = new Location(location, geoData);
+  return locationObj;
+
+}
+
+function Location(city, geoData){
+  this.search_query = city;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+
 app.listen(PORT, () => console.log(`app is listening on port ${PORT}!`))
+
